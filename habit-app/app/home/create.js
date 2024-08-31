b11cc3f18dcc003cfe9912cd602ff2ac8ba1c7e5
Brian Yoon <brianyoon678@gmail.com>
2024-08-31 03:17:17 -0500

    Adding Habits Page Base

--- habit-app/app/home/create.js
+++ habit-app/app/home/create.js
@@ -1,10 +1,53 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native'
 import React from 'react'
+import Ionicons from '@expo/vector-icons/Ionicons';
+import FontAwesome from '@expo/vector-icons/FontAwesome';
+//import { TouchableOpacity } from 'react-native-gesture-handler';
 
 const create = () => {
+  const colors = [
+    "#FF5733", // Red
+    "#FFD700", // Gold
+    "#5D76A9",
+    "#1877F2", // Medium Purple
+    "#32CD32", // Lime Green
+    "#CCCCFF", // Tomato
+    "#4169E1", // Royal Blue
+  ]
   return (
-    <View>
-      <Text>create</Text>
+    <View style={{ padding: 10 }}>
+      {/*Back Icon*/}
+      <Ionicons name="arrow-back" size={24} color="black" />
+      {/*Habit Text*/}
+      <Text style={{ fontSize: 20, marginTop: 10 }}>
+        Create <Text style={{ fontSize: 20, fontWeight: "500" }}>Habit </Text>
+      </Text>
+
+      {/*Habit Name Input*/}
+      <TextInput
+        style={{
+          width: "95%",
+          marginTop: 15,
+          padding: 15,
+          borderRadius: 10,
+          backgroundColor: "#E1EBEE"
+        }}
+        placeholder="Title" />
+
+      {/*Colors for Habit*/}
+      <View style={{ marginVertical: 10 }}>
+        <Text style={{ fontSize: 18, fontWeight: "500" }}>Color</Text>
+        <View style={{ flexDirection: "row", alignItems: "center", gap: 10, marginTop: 10 }}>
+          {colors?.map((item, index) => (
+            <TouchableOpacity key={index} activeOpacity={0.8}>
+              <FontAwesome name="square" size={30} color={item} />
+            </TouchableOpacity>
+          ))}
+        </View>
+      </View>
+      
+      <Text style={{fontSize:18,fontWeight:"500"}}>Repeat</Text>
+      
     </View>
   )
 }
--- habit-app/app/home/index.js
+++ habit-app/app/home/index.js
@@ -1,5 +1,6 @@
 import { StyleSheet, Text, View, ScrollView, Pressable } from 'react-native';
-import React from 'react';
+import React, { useState } from 'react';
+import {useRouter} from "expo-router";
 
 // import icons
 import Ionicons from '@expo/vector-icons/Ionicons';
@@ -7,44 +8,64 @@ import AntDesign from '@expo/vector-icons/AntDesign';
 
 
 const index = () => {
+  const [option, setOption] = useState("Today");
+  const router = useRouter();
+
   return (
     <ScrollView style={{ flex: 1, backgroundColor: "white", padding: 10 }}>
 
-      {/* foursquare logo icon*/} 
+      {/* foursquare logo and plus icon on top as flex direction*/}
       <View style={{ flexDirection: "row", alignItems: "center", justifyContent: "space-between" }}>
+        {/*foursquare logo*/}
         <Ionicons name="logo-foursquare" size={27} color="black" />
-        <AntDesign name="plus" size={24} color="black" />
+
+        {/*plus button logo*/}
+        <AntDesign onPress={() => router.push("/home/create")} name="plus" size={24} color="black" />
+
       </View>
 
       {/*Habits text */}
       <Text style={{ marginTop: 5, fontSize: 23, fontWeight: "500" }}> Habits </Text>
 
       {/*flex direction organization of the pressables: today, weekly, and overall */}
-      <View style={{flexDirection:"row", alignItems:"center", gap:10, marginVertical:8}}>
-        <Pressable style={{
-          backgroundColor: "#E0E0E0",
-          paddingHorizontal: 10,
-          paddingVertical: 8,
-          borderRadius: 25,
-        }}>
+      <View style={{ flexDirection: "row", alignItems: "center", gap: 10, marginVertical: 8 }}>
+
+
+        {/* Today Pressable */}
+        <Pressable
+          onPress={() => setOption("Today")}
+          style={{
+            backgroundColor: option == "Today" ? "#E0FFFF" : "transparent",
+            paddingHorizontal: 10,
+            paddingVertical: 8,
+            borderRadius: 25,
+          }}>
           <Text style={{ textAlign: "center", color: "gray", fontSize: 14 }}>Today</Text>
         </Pressable>
 
-        <Pressable style={{
-          backgroundColor: "#E0E0E0",
-          paddingHorizontal: 10,
-          paddingVertical: 8,
-          borderRadius: 25,
-        }}>
+
+        {/* Weekly Pressable */}
+        <Pressable
+          onPress={() => setOption("Weekly")}
+          style={{
+            backgroundColor: option == "Weekly" ? "#E0FFFF" : "transparent",
+            paddingHorizontal: 10,
+            paddingVertical: 8,
+            borderRadius: 25,
+          }}>
           <Text style={{ textAlign: "center", color: "gray", fontSize: 14 }}>Weekly</Text>
         </Pressable>
 
-        <Pressable style={{
-          backgroundColor: "#E0E0E0",
-          paddingHorizontal: 10,
-          paddingVertical: 8,
-          borderRadius: 25,
-        }}>
+
+        {/*Overall Pressable*/}
+        <Pressable
+          onPress={() => setOption("Overall")}
+          style={{
+            backgroundColor: option == "Overall" ? "#E0FFFF" : "transparent",
+            paddingHorizontal: 10,
+            paddingVertical: 8,
+            borderRadius: 25,
+          }}>
           <Text style={{ textAlign: "center", color: "gray", fontSize: 14 }}>Overall</Text>
         </Pressable>
       </View>
